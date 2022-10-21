013e93cb7668fe60879b7d437185c1bd6937cc3f
Lina Kutitskaya <kutitckaya.lina@yandex.ru>
2022-10-21 07:17:36 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from people import y
+from salary import x
+from datetime import date
+
+d = date(2022, 10, 21)
+
+print(x, y, d)
+
+
+print(__name__)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
